293646f710c75c6c92caaf8d42fda988fa1e7411
Aaron <ad3m3r5@gmail.com>
2024-02-28 17:22:18 -0500

    Update easypki.go

--- pkg/easypki/easypki.go
+++ pkg/easypki/easypki.go
@@ -23,8 +23,8 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/google/easypki/pkg/certificate"
-	"github.com/google/easypki/pkg/store"
+	"github.com/ad3m3r5/easypki/pkg/certificate"
+	"github.com/ad3m3r5/easypki/pkg/store"
 )
 
 const (
