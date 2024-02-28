286188b32e42aea7ac4bdcf68d6070c025308e52
Aaron <ad3m3r5@gmail.com>
2024-02-28 17:21:50 -0500

    Update client-auth.go

--- example/client-auth/client-auth.go
+++ example/client-auth/client-auth.go
@@ -12,9 +12,9 @@ import (
 
 	"github.com/boltdb/bolt"
 	"github.com/go-yaml/yaml"
-	"github.com/google/easypki/pkg/certificate"
-	"github.com/google/easypki/pkg/easypki"
-	"github.com/google/easypki/pkg/store"
+	"github.com/ad3m3r5/easypki/pkg/certificate"
+	"github.com/ad3m3r5/easypki/pkg/easypki"
+	"github.com/ad3m3r5/easypki/pkg/store"
 )
 
 func main() {
